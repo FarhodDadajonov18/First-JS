7f81caaf69567ac83cbb543554074039a3cb8abb
dadajonovfarhod <dadajonovfarhod5@gmail.com>
2022-07-30 09:30:00 +0500

    added some Javascript code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 
 var firstName = prompt("Ismingizni kiriting"),
-lastName = prompt ("Familyangizni kiriting"),
-birthDate = prompt ("Tug'ilgan kuningizni kiriting"),
-email= prompt (" Foydalanuvchining email pochtasi"),
-address= prompt ( "Foydalanuvchining manzili"),
-userNum = prompt("Foydalanuvchining nomeri"),
-workPlace = prompt ("Foydalanuvchining ish joyi"),
-profession =prompt ("Foydalanuvchining kasbi"),
-hobby =prompt ("Foydalanuvchining sevimli mashg'uloti"),
-car = prompt("Foydalanuvchining  moshinasi"),
-text = alert ("Malumotiz uchun rahmat");
+    lastName = prompt ("Familyangizni kiriting"),
+    birthDate = prompt ("Tug'ilgan kuningizni kiriting"),
+    email= prompt (" Foydalanuvchining email pochtasi"),
+    address= prompt ( "Foydalanuvchining manzili"),
+    userNum = prompt("Foydalanuvchining nomeri"),
+    workPlace = prompt ("Foydalanuvchining ish joyi"),
+    profession =prompt ("Foydalanuvchining kasbi"),
+    hobby =prompt ("Foydalanuvchining sevimli mashg'uloti"),
+    car = prompt("Foydalanuvchining  moshinasi"),
+    text = alert ("Malumotiz uchun rahmat");
 
-console.log(
+  console.log(
  
-`Foydalanuvchining ismi ${firstName}
+  ` Foydalanuvchining ismi ${firstName}
 
-Foydalanuvchining familyasi ${lastName} 
+    Foydalanuvchining familyasi ${lastName} 
 
-Foydalanuvchining tug'ilgan yili ${birthDate} 
+    Foydalanuvchining tug'ilgan yili ${birthDate} 
 
-Foydalanuvchining email pochtasi ${email}
+    Foydalanuvchining email pochtasi ${email}
 
-Foydalanuvchining manzili ${address}   
+    Foydalanuvchining manzili ${address}   
 
-Foydalanuvchining nomeri ${userNum}
+    Foydalanuvchining nomeri ${userNum}
 
-Foydalanuvchining kasbi ${profession}
+    Foydalanuvchining kasbi ${profession}
 
-Foydalanuvchining sevimli mashg'uloti ${hobby}
+    Foydalanuvchining sevimli mashg'uloti ${hobby}
 
-Foydalanuvchining moshinasi ${car}
+    Foydalanuvchining moshinasi ${car}
 
-
-`);
+    `
+    );
